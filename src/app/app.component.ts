2ff88bac54bb07a413e3eaf610f41f81f0937f60
testyeduj <88479591+testyeduj@users.noreply.github.com>
2021-09-02 12:08:22 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-/*import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,3 @@
 export class AppComponent {
   title = 'my-first-project';
 }
-*/
