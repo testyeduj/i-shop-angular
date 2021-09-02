643a3773942ffb1e3120627338a1c90e21c590a6
testyeduj <88479591+testyeduj@users.noreply.github.com>
2021-09-02 11:19:22 +0200

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-/*describe('AppComponent', () => {
+describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -19,7 +19,7 @@ import { AppComponent } from './app.component';
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-*/
+
   it(`should have as title 'my-first-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
