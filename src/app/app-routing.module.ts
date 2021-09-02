e5229b29712fce233dc063b9c5ea585f139f77ac
testyeduj <88479591+testyeduj@users.noreply.github.com>
2021-09-02 10:46:41 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+/*import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component'
@@ -25,3 +25,4 @@ const routes: Routes = [{
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+*/
