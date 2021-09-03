4980b54d71b0b8189c30db2b746745f32297f267
testyeduj <88479591+testyeduj@users.noreply.github.com>
2021-09-03 12:44:00 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ShoopingCartService } from './services/shooping-cart.service';
 import { OrderService } from './services/order.service';
 
-@NgModule({
+/*@NgModule({
   declarations: [
     AppComponent,
     ShoppingCartComponent,
@@ -20,8 +20,8 @@ import { OrderService } from './services/order.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
-  ],
+    HttpClientModule*/
+    ],
   providers: [
     ProductService, 
     ShoopingCartService,
